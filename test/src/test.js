fa7e090552381f77086ed50d8fb3db436a4eaac6
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-03-03 23:04:29 +0100

    Tests: optionally pass options to test function

--- test/src/test.js
+++ test/src/test.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
 const assert = require('assert')
 
-module.exports = function test (fun, id, data, callback) {
-  const actual = fun(data)
+module.exports = function test (fun, id, data, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = {}
+  }
+
+  const actual = fun(data, options)
   fs.readFile('test/data/' + id + '.svg', (err, expected) => {
     assert.equal(actual, expected.toString())
     callback(err)
