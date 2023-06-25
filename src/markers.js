87f17455335188e8d2b19218dec8f90c5ae489a4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-06-25 15:19:59 +0200

    don't rely on a getMetersPerPixel() function

--- src/markers.js
+++ src/markers.js
@@ -2,7 +2,11 @@ const parseLength = require('overpass-layer/src/parseLength')
 const getHalfHeight = require('./getHalfHeight')
 
 function metersPerPixel () {
-  return global.map ? global.map.getMetersPerPixel() : 1
+  if (global.map) {
+    return 40075016.686 * Math.abs(Math.cos(global.map.getCenter().lat / 180 * Math.PI)) / Math.pow(2, global.map.getZoom() + 8)
+  }
+
+  return 1
 }
 
 function cssStyle (style) {
