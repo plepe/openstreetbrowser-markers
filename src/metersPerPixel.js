13a4e2c2c663adeef21611ab1f5c52b7f6bd2557
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-05-07 22:05:17 +0200

    metersPerPixel: only use map center if the map object has a getCenter method

--- src/metersPerPixel.js
+++ src/metersPerPixel.js
@@ -1,5 +1,5 @@
 module.exports = function metersPerPixel () {
-  if (global.map) {
+  if (typeof global.map === 'object' && global.map.getCenter && typeof global.map.getCenter === 'function') {
     return 40075016.686 * Math.abs(Math.cos(global.map.getCenter().lat / 180 * Math.PI)) / Math.pow(2, global.map.getZoom() + 8)
   }
 
